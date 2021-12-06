ea891fae631b78a737da475dfa2a23bc2e4ce4d6
Andres Copeticona <andrescopeticona@outlook.es>
2021-12-06 11:40:28 -0400

    index limpio

--- index.js
+++ index.js
@@ -11,9 +11,5 @@ const sumPairs = require('./Challenge06/index');
 	// console.log(isValid('bici coche (balón) bici coche peluche'));
 	// console.log(createXmasTree(3));
 	// console.log(daysToXmas(new Date('Dec 1, 2021')));
-	console.log(sumPairs([3, 5, 7, 2], 10)); // [3, 7]
-	console.log(sumPairs([-3, -2, 7, -5], 10)); // null
-	console.log(sumPairs([2, 2, 3, 1], 4)); // [2, 2]
-	console.log(sumPairs([6, 7, 1, 2], 8)); // [6, 2]
-	console.log(sumPairs([0, 2, 2, 3, -1, 1, 5], 6)); // [1, 5]
+	// console.log(sumPairs([3, 5, 7, 2], 10)); // [3, 7]
 })();
